033fba89dc8dfb52e838ce19628264aa5ab4114c
n67h <night67hunter@gmail.com>
2023-09-21 14:21:53 +0800

    Update index.php
     - Add fetchWeather function

--- index.php
+++ index.php
@@ -8,4 +8,18 @@
 <body>
     <h3>OpenWeatherMap API Demo</h3>
 </body>
+<script>
+    function renderWeather() {
+    }
+
+
+    function fetchWeather(query) {
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=7dc4416dadb5c400047e5cb4b4d77b07"
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
+    fetchWeather();
+</script>
 </html>
