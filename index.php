a46e632fef757609a46ca035c30d097ea04735f8
n67h <night67hunter@gmail.com>
2023-09-22 14:23:05 +0800

    Refactor fetchWeather function
     - Add input type
     - Add search button
     - City temporarily defaults to Manila

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     
     <!-- latest bootstrap cdn -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- font-awesome cdn -->
+    <script src="https://kit.fontawesome.com/3481525a72.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="row">
@@ -19,22 +21,31 @@
             <div class="container mt-5 search-bar">
                 <form method="post" action="">
                     <label for="city" class="form-label fs-3 fw-bold p-2">See the weather details by typing the city name</label>
-                    <input class="form-control form-control-lg" type="text" placeholder="Search..." aria-label="city" name="city" id="city">
-                    <div class="container mt-3 weather-results bg-info" id="weather-results">
-                        
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control form-control-lg" placeholder="Search..." aria-label="city" name="city" id="city" aria-describedby="search">
+                        <button class="btn btn-outline-secondary" type="button" name="search" id="search"><i class="fa-solid fa-magnifying-glass fa-flip-horizontal"></i></button>
+                    </div>
+
+                    <div class="container mt-3 weather-results" id="weather-results">   
+                    <?php
+                        if(!isset($_POST['search'])){
+                            echo '<p class="text-danger">Please search the city that you want.</p>';
+                        }else{
+                            $city = $_POST['city'];
+                        }
+                    ?>
                     </div>
                 </form>
             </div>
         </div>
 
-
         <div class="col-md-4">
 
         </div>
-    </div>
-
-    
+    </div>    
 </body>
+
+
 
 <script>
     function renderWeather(weather) {
@@ -49,7 +60,7 @@
 
         // display p for humidity, wind, description, temp
         var temp = document.createElement("p");
-        temp.textContent = "Temp: " + weather.main.temp + " F";
+        temp.textContent = "Temperature: " + weather.main.temp + " F";
         resultsContainer.append(temp);
 
         var humidity = document.createElement("p");
@@ -63,21 +74,19 @@
         var details = weather.weather[0];
         if (details && details.description){
             var description = document.createElement("p");
-            description.textContent = details.description;
+            description.textContent = "Description: " + details.description;
             resultsContainer.append(description);
         }
-        
     }
 
 
     function fetchWeather(query) {
-        var url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=7dc4416dadb5c400047e5cb4b4d77b07"
-
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=7dc4416dadb5c400047e5cb4b4d77b07"
         fetch(url)
             .then((response) => response.json())
             .then((data) => renderWeather(data));
     }
-    fetchWeather();
+    fetchWeather("Manila");
 </script>
 
 <!-- latest bootstrap js popper -->
